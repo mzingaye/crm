5a6294f6327b3c4855c00f5a9f3a910536b0d3f9
Mzingaye Masilela Win7 <mzingaye.masilela@lynesystems.com>
2017-07-19 17:14:29 +0200

    Additional Features Sprint Debugging Ready - 19/07/2017

--- crm/crm-war/src/java/controllers/MatrimonialController.java
+++ crm/crm-war/src/java/controllers/MatrimonialController.java
@@ -5,10 +5,24 @@
  */
 package controllers;
 
+import beans.CatholicBean;
+import beans.CatholicSpouseBean;
+import beans.MatrimonialBean;
+import beans.SponsorBean;
+import beans.SponsorSpouseBean;
 import entities.Matrimonial;
+import entities.Minister;
+import entities.Parish;
+import entities.User;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.inject.Inject;
+import models.CatholicFacade;
 import models.MatrimonialFacade;
+import models.MinisterFacade;
+import models.ParishFacade;
+import models.SponsorFacade;
+import models.UserFacade;
 
 /**
  *
@@ -16,39 +30,93 @@ import models.MatrimonialFacade;
  */
 public class MatrimonialController {
     @EJB
+    private ParishFacade parishFacade;
+    @EJB
+    private UserFacade userFacade;
+    @EJB
+    private SponsorFacade sponsorFacade;
+    @EJB
+    private CatholicFacade catholicFacade;
+    @EJB
+    private MinisterFacade ministerFacade;
+    
+    @EJB
     private MatrimonialFacade matrimonialFacade;
     
-    private int ministerId;
+    @Inject
+    private MatrimonialBean mBean;
     
-    private int parishId;
-
-    public int getMinisterId() {
-        return ministerId;
-    }
-
-    public void setMinisterId(int ministerId) {
-        this.ministerId = ministerId;
-    }
-
-    public int getParishId() {
-        return parishId;
-    }
-
-    public void setParishId(int parishId) {
-        this.parishId = parishId;
-    }
-
-    private Matrimonial m;
-
-    public Matrimonial getM() {
-        return m;
-    }
-
-    public void setM(Matrimonial m) {
-        this.m = m;
-    }
+    @Inject
+    private CatholicBean cBean;
+    
+    @Inject
+    private CatholicSpouseBean csBean;
+    
+    @Inject 
+    private SponsorBean sBean;
+    
+    @Inject
+    private SponsorSpouseBean ssBean;
     
     public MatrimonialController() {
+    }
+    
+    public String newRec(){
+        sBean.setFname("");
+        sBean.setMname("");
+        sBean.setLname("");
+        sBean.setSex("");
+        sBean.setDob(null);
+        sBean.setAge(0);
+        sBean.setNatID("");
+        sBean.setContact("");
+        
+        ssBean.setFname("");
+        ssBean.setMname("");
+        ssBean.setLname("");
+        ssBean.setSex("");
+        ssBean.setDob(null);
+        ssBean.setAge(0);
+        ssBean.setNatID("");
+        ssBean.setContact("");
+        
+        cBean.setFname("");
+        cBean.setMname("");
+        cBean.setLname("");
+        cBean.setSex("");
+        cBean.setDob(null);
+        cBean.setAge(0);
+        cBean.setPlaceOfBirth("");
+        cBean.setNatID("");
+        cBean.setContact("");
+        cBean.setFfname("");
+        cBean.setFmname("");
+        cBean.setFlname("");
+        cBean.setFnatID("");
+        cBean.setMfname("");
+        cBean.setMmname("");
+        cBean.setMlname("");
+        cBean.setMnatID("");
+        
+        csBean.setFname("");
+        csBean.setMname("");
+        csBean.setLname("");
+        csBean.setSex("");
+        csBean.setDob(null);
+        csBean.setAge(0);
+        csBean.setPlaceOfBirth("");
+        csBean.setNatID("");
+        csBean.setContact("");
+        csBean.setFfname("");
+        csBean.setFmname("");
+        csBean.setFlname("");
+        csBean.setFnatID("");
+        csBean.setMfname("");
+        csBean.setMmname("");
+        csBean.setMlname("");
+        csBean.setMnatID("");
+        
+        return "creatematrimonialrec";
     }
     
     public List<Matrimonial> getAll(){
@@ -56,7 +124,29 @@ public class MatrimonialController {
     }
     
     public String add(){
-        return null;
+        Matrimonial m = new Matrimonial();
+        m.setMarriageNumber(mBean.getMarriageNumber());
+        m.setDateOfMarriage(mBean.getDateOfMarriage());
+        m.setHusbandMemberID(cBean.getId());
+        m.setHusbandAddress(mBean.getHusbandAddress());
+        m.setWifeMemberID(csBean.getId());
+        m.setWifeAddress(mBean.getWifeAddress());
+        m.setHusbandSponsorID(sBean.getId());
+        m.setWifeSponsorID(ssBean.getId());
+        m.setOfficialDesignation(mBean.getOfficialDesignation());
+        m.setConditionOfHus(mBean.getConditionOfHus());
+        m.setConditionOfWife(mBean.getConditionOfWife());
+        m.setConsentHus(mBean.getConsentHus());
+        m.setConsentWife(mBean.getConsentWife());
+        m.setMarriageBy(mBean.getMarriageBy());
+        Parish p = this.parishFacade.find(mBean.getParishid());
+        Minister min = this.ministerFacade.find(mBean.getMinisterid());
+        User u = this.userFacade.find(2);
+        m.setParishid(p);
+        m.setMinisterid(min);
+        m.setUserid(u);
+        this.matrimonialFacade.create(m);
+        return "matrimonial";
     }
     
 }
