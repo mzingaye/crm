63e11c44e8fc66059a3392057bb1587a69aed251
Mzingaye Masilela Win7 <mzingaye.masilela@lynesystems.com>
2017-07-19 17:08:18 +0200

    Additional Features Sprint Debugging - 19/07/2017

--- crm/crm-war/src/java/controllers/SponsorController.java
+++ crm/crm-war/src/java/controllers/SponsorController.java
@@ -6,6 +6,7 @@
 package controllers;
 
 import beans.SponsorBean;
+import beans.SponsorSpouseBean;
 import entities.Sponsor;
 import java.util.List;
 import javax.ejb.EJB;
@@ -22,6 +23,9 @@ public class SponsorController {
     
     @Inject
     private SponsorBean sBean;
+    
+    @Inject
+    private SponsorSpouseBean ssBean;
     
     public SponsorController() {
         
@@ -63,6 +67,28 @@ public class SponsorController {
     }
     
     public String search(){
+        Sponsor s = this.sponsorFacade.find(sBean.getId());
+        sBean.setFname(s.getFname());
+        sBean.setMname(s.getMname());
+        sBean.setLname(s.getMname());
+        sBean.setSex(s.getSex());
+        sBean.setDob(s.getDob());
+        sBean.setAge(s.getAge());
+        sBean.setNatID(s.getNatID());
+        sBean.setContact(s.getContact());
+        return null;
+    }
+    
+    public String searchSpouse(){
+        Sponsor ss = this.sponsorFacade.find(ssBean.getId());
+        ssBean.setFname(ss.getFname());
+        ssBean.setMname(ss.getMname());
+        ssBean.setLname(ss.getMname());
+        ssBean.setSex(ss.getSex());
+        ssBean.setDob(ss.getDob());
+        ssBean.setAge(ss.getAge());
+        ssBean.setNatID(ss.getNatID());
+        ssBean.setContact(ss.getContact());
         return null;
     }
 }
